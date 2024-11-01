a98699cdbbe88862dcc080a08251f77c9679d750
Turbobot <turbobot@vercel.com>
2024-11-01 09:04:54 +0800

    feat(create-turbo): apply official-starter transform

--- packages/logger/src/types.ts
+++
@@ -1,6 +0,0 @@
-export interface LoggerOptions {
-  showTimestamp: boolean
-  level: LogLevel
-}
-
-export type LogLevel = 'info' | 'debug' | 'warn' | 'error'
--- packages/typescript-config/README.md
+++
@@ -1,2 +0,0 @@
-# `@vani0-0/typescript-config`
- These are base shared `tsconfig.json`s from which all other tsconfig.json's inherit from.
